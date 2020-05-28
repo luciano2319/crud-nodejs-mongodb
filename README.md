0c742c674941946029316642856fca67eebe7ee7
luciano2319 <lfcc_1369@hotmail.com>
2020-05-28 17:57:27 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# CRUD NODE APP
+
+## Environment variables
+* PORT, this is server port by default is 4000
+* NOTES_APP_MONGODB_HOST=localhost
+* NOTES_APP_MONGODB_DATABASE=notes-app
---
+++ src/controllers/users.controllers.js
@@ -0,0 +1,59 @@
+const usersCtrl = {};
+
+const passport = require('passport');
+
+usersCtrl.renderSignUpForm = (req, res) => {
+    res.render('users/signup');
+};
+
+const User = require('../models/User');
+
+usersCtrl.signup = async (req, res) => {
+    const errors = [];
+    const { name, email, password, confirm_password } = req.body;
+    if(password!=confirm_password){
+        errors.push({text: 'Passwords do not match'});
+    }
+    if(password.length < 4){
+        errors.push({text: 'Passwords must be at least 4 characters'});
+    }
+    if(errors.length>0){
+        res.render('users/signup' , {
+            errors,
+            name,
+            email
+        });
+    } else {
+        const emailUser = await User.findOne({ email: email});
+        if(emailUser){
+            req.flash('error_msg', 'The email is alredy in use');
+            res.redirect('/users/signup');
+        } else {
+            const newUser = new User({ name, email, password});
+            newUser.password = await newUser.encryptPassword(password);
+            await newUser.save();
+            req.flash('success_msg', 'Estás registrado');
+            res.redirect('/users/signin');
+        }
+    }
+};
+
+usersCtrl.renderSignInForm = (req,res) => {
+    res.render('users/signin');
+};
+
+usersCtrl.signin = passport.authenticate('local', {
+    failureRedirect: '/users/signin',
+    successRedirect: '/notes',
+    failureFlash: true
+});
+
+usersCtrl.logout = (req, res) => {
+    req.logout();
+    req.flash('success_msg',"You're logged out now");
+    res.redirect('/users/signin');
+};
+
+
+
+module.exports = usersCtrl;
